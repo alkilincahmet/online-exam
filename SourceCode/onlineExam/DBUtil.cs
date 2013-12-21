baac0a4409c8adaf002a18fff67da55004680d12
Ahmet ALKILINÇ <alkilinc_ahmet@hotmail.com>
2013-12-21 23:01:17 +0200

    DBUtil.cs eklendi

---
+++ SourceCode/onlineExam/DBUtil.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+using System.Web.Configuration;
+
+namespace onlineExam
+{
+    class DBUtil
+    {
+        public static String ConnectionString
+        {
+            get
+            {
+                ConnectionStringSettings constr = WebConfigurationManager.ConnectionStrings["examConnectionString"];
+                return constr.ConnectionString;
+            }
+
+        }
+    }
+}
