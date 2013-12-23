dac90c63859fa5d2b43da38637eff3cd85392831
enesbatur <enes@arilaryazilim.com>
2013-12-23 21:59:19 +0200

    Default.aspx.cs update

--- SourceCode/onlineExam/Default.aspx.cs
+++ SourceCode/onlineExam/Default.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace onlineExam
 {
@@ -11,6 +13,30 @@ namespace onlineExam
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                GetExam();
+            }
+        }
+
+
+
+        void GetExam()
+        {
+            SqlConnection con = new SqlConnection(DBUtil.ConnectionString);
+            try
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("select  DISTINCT top 10 e.* from tbl_Exams e,tbl_Questions q WHERE e.ExamID=q.ExamID order by e.ExamID desc", con);
+                SqlDataAdapter a = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                a.Fill(dt);
+
+                repSoru.DataSource = dt;
+                repSoru.DataBind();
+            }
+            catch { }
+
 
         }
     }
