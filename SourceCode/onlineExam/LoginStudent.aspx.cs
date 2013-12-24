41cf5cd2e629e26dbeba1cf330e5a867c4473b7f
abdullahazarkan <abdullah.azarkan@hotmail.com>
2013-12-24 18:17:19 +0200

    LoginStudent.aspx.cs güncellendi
    güncellendi

--- SourceCode/onlineExam/LoginStudent.aspx.cs
+++ SourceCode/onlineExam/LoginStudent.aspx.cs
@@ -6,6 +6,8 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Data.SqlClient;
 using System.Data;
+using System.Web.Services;
+using System.Web.Script.Services;
 
 namespace onlineExam
 {
@@ -17,6 +19,29 @@ namespace onlineExam
 
         }
 
+        [WebMethod]
+        [ScriptMethod]
+        public static void SaveUser(User user)
+        {
+            SqlConnection con = new SqlConnection(DBUtil.ConnectionString);
+            using (SqlCommand cmd = new SqlCommand("INSERT INTO tbl_Users (UserName,Password,FullName,Email,Authory) VALUES(@Username, @Password,@FullName,@Email,@Authory)"))
+            {
+                cmd.CommandType = CommandType.Text;
+                cmd.Parameters.AddWithValue("@Username", user.Username);
+                cmd.Parameters.AddWithValue("@Password", user.Password);
+                cmd.Parameters.AddWithValue("@FullName", user.FullName);
+                cmd.Parameters.AddWithValue("@Email", user.Email);
+                cmd.Parameters.AddWithValue("@Authory", 2);
+
+
+                cmd.Connection = con;
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+
+        }
+
         protected void btnLogin_Click(object sender, EventArgs e)
         {
 
@@ -25,7 +50,7 @@ namespace onlineExam
             try
             {
                 con.Open();
-                SqlCommand cmd = new SqlCommand("select * from tbl_Users where UserName = @lname and Password = @pwd", con);
+                SqlCommand cmd = new SqlCommand("select * from tbl_Users where UserName = @lname and Password = @pwd and Authory=2", con);
                 cmd.Parameters.Add("@lname", SqlDbType.VarChar, 10).Value = txtUsername.Value;
                 cmd.Parameters.Add("@pwd", SqlDbType.VarChar, 10).Value = txtPassword.Value;
 
@@ -45,7 +70,7 @@ namespace onlineExam
                 }
                 else
                 {
-                    lblUyari.Text = "<span class=\"fail\">Invalid login. &nbsp;&nbsp;<a class=\"page-back\" href=\"#\">Try again?</a></span>";
+                    lblUyari.Text = "<span class=\"fail\">Invalid login. &nbsp;&nbsp; Username or Password Incorrect.</span>";
 
                     dr.Close();
                 }
