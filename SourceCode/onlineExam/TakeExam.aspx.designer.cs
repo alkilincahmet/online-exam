b60ef4fcc4fd5276f0dc050b6dd905560ce1bb60
enesbatur <enes@arilaryazilim.com>
2013-12-31 16:52:32 +0200

    TakeExam.aspx.designer.cs update

--- SourceCode/onlineExam/TakeExam.aspx.designer.cs
+++ SourceCode/onlineExam/TakeExam.aspx.designer.cs
@@ -22,12 +22,21 @@ namespace onlineExam {
         protected global::System.Web.UI.WebControls.DropDownList dropCategory;
         
         /// <summary>
-        /// dropSOrular control.
+        /// dropExams control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.DropDownList dropSOrular;
+        protected global::System.Web.UI.WebControls.DropDownList dropExams;
+        
+        /// <summary>
+        /// btnStart control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnStart;
     }
 }
