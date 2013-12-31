da1d42e86622fcc9c80009799e7318f94132d347
enesbatur <enes@arilaryazilim.com>
2013-12-31 16:52:04 +0200

    TakeExam.aspx.cs Update

--- SourceCode/onlineExam/TakeExam.aspx.cs
+++ SourceCode/onlineExam/TakeExam.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace onlineExam
 {
@@ -11,7 +13,80 @@ namespace onlineExam
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                GetExamCategory();
+                GetExam();
+
+                if (Request.QueryString["cid"] != null)
+                {
+                    dropCategory.SelectedValue = Request.QueryString["cid"];
+                }
+
+                if (Request.QueryString["eid"] != null)
+                {
+                    dropExams.SelectedValue = Request.QueryString["eid"];
+                }
+            }
 
         }
+
+        void GetExamCategory()
+        {
+            SqlConnection con = new SqlConnection(DBUtil.ConnectionString);
+            try
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("select * from tbl_Categories order by CategoryName", con);
+
+
+                SqlDataAdapter a = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                a.Fill(dt);
+
+
+                dropCategory.DataTextField = "CategoryName";
+                dropCategory.DataValueField = "CategoryID";
+
+                dropCategory.DataSource = dt;
+                dropCategory.DataBind();
+                dropCategory.Items.Insert(0, new ListItem("All", "0"));
+
+            }
+            catch { }
+
+
+        }
+
+        void GetExam()
+        {
+            SqlConnection con = new SqlConnection(DBUtil.ConnectionString);
+            try
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("select DISTINCT e.* from tbl_Exams e,tbl_Questions q WHERE e.ExamID=q.ExamID order by e.ExamName", con);
+
+
+                SqlDataAdapter a = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                a.Fill(dt);
+
+
+                dropExams.DataTextField = "ExamName";
+                dropExams.DataValueField = "ExamID";
+
+                dropExams.DataSource = dt;
+                dropExams.DataBind();
+                dropExams.Items.Insert(0, new ListItem("All", "0"));
+
+            }
+            catch (Exception) { }
+
+
+        }
+
+
+
+
     }
 }
