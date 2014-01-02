39856db927e364e60381da6ce447c2f6302505f3
enesbatur <enes@arilaryazilim.com>
2014-01-02 14:17:55 +0200

    Question.cs update

--- SourceCode/onlineExam/Question.cs
+++ SourceCode/onlineExam/Question.cs
@@ -8,7 +8,7 @@ namespace onlineExam
     public class Question
     {
         public String question;
-        public String ans1, ans2, ans3, ans4, ans5, cans, answer;
+        public String ans1, ans2, ans3, ans4, ans5, cans, answer,imgUrl;
 
         public String QuestionText
         {
@@ -49,10 +49,13 @@ namespace onlineExam
         {
             get { return answer; }
         }
+        public String ImgUrl
+        {
+            get { return imgUrl; }
+        }
 
 
-
-        public Question(String question, String ans1, String ans2, String ans3, String ans4, String ans5, String cans)
+        public Question(String question, String ans1, String ans2, String ans3, String ans4, String ans5, String cans,string imgUrl)
         {
             this.question = question;
             this.ans1 = ans1;
@@ -61,6 +64,7 @@ namespace onlineExam
             this.ans4 = ans4;
             this.ans5 = ans5;
             this.cans = cans;
+            this.imgUrl = imgUrl;
         }
 
 
