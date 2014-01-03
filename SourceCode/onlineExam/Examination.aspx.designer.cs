922900e6d78022874fd4e3546d0eda4c661cda12
Ahmet ALKILINÇ <alkilinc_ahmet@hotmail.com>
2014-01-03 17:54:33 +0200

    Examination.aspx.designer.cs güncellendi

--- SourceCode/onlineExam/Examination.aspx.designer.cs
+++ SourceCode/onlineExam/Examination.aspx.designer.cs
@@ -49,6 +49,15 @@ namespace onlineExam {
         protected global::System.Web.UI.WebControls.Label lblCurrentTime;
         
         /// <summary>
+        /// lblImage control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblImage;
+        
+        /// <summary>
         /// question control.
         /// </summary>
         /// <remarks>
