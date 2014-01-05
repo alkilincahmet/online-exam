b95d043a2e2635486cec18608ee66eb48ea3f32f
abdullahazarkan <abdullah.azarkan@hotmail.com>
2014-01-05 13:17:30 +0200

    ReviewQuestions.aspx.cs

---
+++ SourceCode/onlineExam/ReviewQuestions.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace onlineExam
+{
+    public partial class ReviewQuestions : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                if (Session["questions"] != null)
+                {
+                    Examinationn exam = (Examinationn)Session["questions"];
+                    datalistQuestion.DataSource = exam.questions;
+                    datalistQuestion.DataBind();
+                    
+
+                }
+               
+            }
+        }
+
+    }
+}
